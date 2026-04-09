935c4f614dff36300555c91c26b1176557369ce2
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2026-04-09 22:23:28 +0530

    added

--- server/models/BuyersSchema.js
+++ server/models/BuyersSchema.js
@@ -1,35 +1,35 @@
- 
+
 import mongoose from "mongoose";
 export const buyersSchema = new mongoose.Schema({
-    fullName:{
-        type:String,
-        required:true,
+    fullName: {
+        type: String,
+        required: true,
 
     },
-    age:{
-        type:Number,
-        required:true,
+    age: {
+        type: Number,
+        required: true,
     },
-    address:{
-        type:String,
-        required:true,
+    address: {
+        type: String,
+        required: true,
     },
-    phoneNumber:{
-        type:Number,
-        required:true,
-        unique:true,
+    phoneNumber: {
+        type: Number,
+        required: true,
+
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
 
     }
 
-},{timestamps:true});
-const buyersmodel1=mongoose.model("Buyersschema",buyersSchema);
+}, { timestamps: true });
+const buyersmodel1 = mongoose.model("Buyersschema", buyersSchema);
 export default buyersmodel1;
